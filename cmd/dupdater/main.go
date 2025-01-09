81dd604b8ce39e44cc3e8fb1a23163b3897d0bbe
박기현 <kihyun1998@naver.com>
2025-01-09 17:20:15 +0900

    Fix: 복구 로직 개선

--- cmd/dupdater/main.go
+++ cmd/dupdater/main.go
@@ -120,5 +120,6 @@ func getCurrentDir() string {
 		fmt.Printf("Failed to get executable path: %v\n", err)
 		os.Exit(1)
 	}
-	return filepath.Dir(dir)
+	// dupdater 폴더가 아닌 상위 디렉토리를 반환
+	return filepath.Dir(filepath.Dir(dir))
 }
